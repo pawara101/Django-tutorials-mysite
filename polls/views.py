eeb0e5c2e45d3762227289d02386025ef253767e
pawara101 <pawara101dassanayake@gmail.com>
2023-01-16 16:24:38 +0530

    raise 404 exeption | Part 3 |2023-01-16

--- polls/views.py
+++ polls/views.py
@@ -1,17 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Question
-
+from django.http import Http404
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     context = {'latest_question_list': latest_question_list}
     return render(request, 'polls/index.html', context)
 
-
-# Leave the rest of the views (detail, results, vote) unchanged
+## The view raises the Http404 exception if a question with the requested ID doesn’t exis
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
